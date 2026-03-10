419ebc2e2eec00af7f3b3f4a92a100bb3091195e
EricYunWang <ericwang3@hotmail.com>
2026-03-10 01:40:06 +0000

    Implement basic calculation functionality
    The buttons now works, and clicked numbers will show on the display.
    The calculator now works, but only if equal button is pressed after
    each operation. For example:
    1. n1 * n2 = n3 clear n2 * n3 = n4. Works.
    2. n1 * n2 = n3 * n4 = n5. Works.
    3. n1 * n2 * n3 does not work. This skips n1. answer is n2 * n3.

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let a, operator, b;
+const numberButtons = document.querySelectorAll("#numButton");
+const display = document.querySelector("#display");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const times = document.querySelector("#times");
+const divides = document.querySelector("#divide");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
 
 function add(a, b){
     return a + b;
@@ -29,6 +37,56 @@ function operate(a, operator, b){
     }
 }
 
+numberButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    let value = event.target.textContent;
+    display.textContent += value;
+  });
+});
+
+plus.addEventListener('click',() =>{
+    a = display.textContent;
+    operator = '+';
+    display.textContent = "";
+    console.log(a);
+})
+
+minus.addEventListener('click',() =>{
+    a = display.textContent;
+    operator = '-';
+    display.textContent = "";
+    console.log(a);
+})
+
+times.addEventListener('click',() =>{
+    a = display.textContent;
+    operator = '*';
+    display.textContent = "";
+    console.log(a);
+})
+
+divides.addEventListener('click',() =>{
+    a = display.textContent;
+    operator = '/';
+    display.textContent = "";
+    console.log(a);
+})
+
+equal.addEventListener('click',() =>{
+    b = display.textContent;
+    a = Number(a);
+    b = Number(b);
+    console.log(a, b, operator);
+    let c = operate(a, operator, b);
+    display.textContent = c;
+    a = c;
+})
+
+clear.addEventListener('click',() =>{
+    display.textContent = "";
+    a = "";
+    b = "";
+})
 
 
-console.log(operate(3, '*', 2));
+console.log(operate(1, '+', 2));
